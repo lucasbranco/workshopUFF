7a6c52ce01e9f45da146158dfbc63e101cb48965
lucasbranco <lucasmbranco@gmail.com>
2016-03-11 17:46:30 -0300

    dsdas

--- js/alunos.js
+++ js/alunos.js
@@ -1,12 +1,12 @@
-var alunos = [
-    /*{
+﻿var alunos = [
+    {
         "nome": "wladek zacharski dos Santos",
         "curso": "Ciência da Computação",
         "mensagem": "Ta legal!"
     },
     {
-        "nome": "Fulando de tal",
-        "curso": "Outra coisas",
-        "mensagem": "Muito show"
-    }*/
+        "nome": "Astronauta",
+        "curso": "Ufologia",
+        "mensagem": "Adoro E.T."
+    }
 ];
